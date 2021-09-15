ec13feaa5d0a2eed0ce0c872df5c03fc8565b89f
daugstinsh37126 <lbi.janis@inbox.lv>
2021-09-15 13:35:33 +0300

    HomeWork2 2

--- HomeWork2/HomeWork2/Program.cs
+++ HomeWork2/HomeWork2/Program.cs
@@ -51,14 +51,56 @@ namespace HomeWork2
             Console.WriteLine("Ievadiet divus skaitļus. Noteiksim mazāko skaitli :)");
 
             string MyNumbertext12 = Console.ReadLine();
-            int MyNumber2 = int.Parse(MyNumbertext10);
+            int MyNumber2 = int.Parse(MyNumbertext12);
             string MyNumbertext13 = Console.ReadLine();
-            int MyNumber3 = int.Parse(MyNumbertext11);
+            int MyNumber3 = int.Parse(MyNumbertext13);
             int smallest = Math.Max(MyNumber2, MyNumber3);
             Console.WriteLine(smallest);
 
             // ---------------------------------------------------------------;
             Console.WriteLine("5.uzdevums");
+            Console.WriteLine("Ievadiet divus skaitļus. Noteiksim skaitļu dalījumu.");
+
+            string MyNumbertext15 = Console.ReadLine();
+            int divisionReminder1 = int.Parse(MyNumbertext15);
+            string MyNumbertext16 = Console.ReadLine();
+            int divisionReminder2 = int.Parse(MyNumbertext16);
+            int divisionreminderResult = divisionReminder1 % divisionReminder2;
+            Console.WriteLine(divisionreminderResult);
+
+            //-----------------------------------------------------------------;
+            Console.WriteLine("6.uzdevums");
+            Console.WriteLine("Ievadiet skaitli. Noteiksim pāra vai nepāra skaitli ievadījāt?.");
+
+            Console.WriteLine(int.Parse(Console.ReadLine()) 
+            % 2 == 0 ? "pāra" : "nepāra");
+            //----------------------------------------------------------------;
+            Console.WriteLine("7.uzdevums");
+            Console.WriteLine("Ievadiet taisnleņķa trīsstūra sānu malu garumus!");
+
+            double side1, side2, area;
+            Console.Write("Mala 1 : ");
+            side1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Mala 2 : ");
+            side2 = Convert.ToDouble(Console.ReadLine());
+            area = (side1 * side2) / 2;
+            Console.WriteLine("Taisnleņķa laukums : " + area);
+            Console.ReadKey();
+
+            //------------------------------------------------------------------;
+            Console.WriteLine("8.uzdevums");
+            Console.WriteLine("Vienādsānu trijstūra pamatme garums 5. Lūdzu ievadīet augstumu un tiks aprēķināts laukums trijstūrim!");
+
+            double side3, height, area1;
+            Console.ReadLine("Pamatne : 5");
+            side1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Augstums : ");
+            height = Convert.ToDouble(Console.ReadLine());
+            area1 = (side1 * height) / 2;
+            Console.WriteLine("Vienādsānu trijstūra laukums : " + area1);
+            Console.ReadKey();
+
+            //-----------------------------------------------------------------;
 
 
 
